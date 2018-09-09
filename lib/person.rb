c30085aa5e11769ea329d37ee9903217d7947ca0
Kathleen Howard <kmaryhoward@gmail.com>
2018-09-09 19:44:01 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,24 +3,25 @@ class Person
   def initialize(name)
     @name = name
   end
-
-  def name
-    @name
-  end
-
+  
   def name=(new_name)
     @name = new_name
+  end
+  
+  def name
+    @name
   end
   
   def initialize(job)
     @job = job
   end
 
+  def job=(new_job)
+    @job = new_job
+  end
+
   def job
     @job
   end
 
-  def job=(new_job)
-    @job = new_job
-  end
 end
